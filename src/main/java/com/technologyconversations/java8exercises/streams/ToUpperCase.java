ca6d51f53a52100a85c1af3f860745d9d14d1b40
Viktor Farcic <viktor.farcic@everis.com>
2014-10-27 12:41:23 +0100

    Added ToUpperCase

--- src/main/java/com/technologyconversations/java8exercises/streams/ToUpperCase.java
+++ src/main/java/com/technologyconversations/java8exercises/streams/ToUpperCase.java
@@ -1,14 +1,23 @@
 package com.technologyconversations.java8exercises.streams;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
 public class ToUpperCase {
 
+    public static List<String> transform7(List<String> collection) {
+        List<String> upperCaseList = new ArrayList<>();
+        for (String element : collection) {
+            upperCaseList.add(element.toUpperCase());
+        }
+        return upperCaseList;
+    }
+
     public static List<String> transform(List<String> collection) {
         return collection.stream() // Convert collection to Stream
-                .map(element -> element.toUpperCase()) // Convert each element to upper case
+                .map(String::toUpperCase) // Convert each element to upper case
                 .collect(toList()); // Collect results to a new list
     }
 
--- src/test/java/com/technologyconversations/java8exercises/streams/ToUpperCaseSpec.java
+++ src/test/java/com/technologyconversations/java8exercises/streams/ToUpperCaseSpec.java
@@ -2,17 +2,19 @@ package com.technologyconversations.java8exercises.streams;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static com.technologyconversations.java8exercises.streams.ToUpperCase.transform;
+import static com.technologyconversations.java8exercises.streams.ToUpperCase.*;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
+/*
+Convert elements of a collection to upper case.
+ */
 public class ToUpperCaseSpec {
 
     @Test
-    public void toUpperCaseShouldConvertCollectionElementsToUpperCase() {
+    public void transformShouldConvertCollectionElementsToUpperCase() {
         List<String> collection = asList("My", "name", "is", "John", "Doe");
         List<String> expected = asList("MY", "NAME", "IS", "JOHN", "DOE");
         assertThat(transform(collection)).hasSameElementsAs(expected);

effdcf7f7105d6f7bb4a35d4021ddc6423e26304
ayman abdelghany <aymana.ghany@devfactory.com>
2016-05-13 14:03:11 +0200

    The members of an interface declaration or class should appear in a pre-defined order

--- src/main/java/com/technologyconversations/java8exercises/streams/Stats.java
+++ src/main/java/com/technologyconversations/java8exercises/streams/Stats.java
@@ -2,6 +2,11 @@ package com.technologyconversations.java8exercises.streams;
 
 public class Stats {
 
+    private long count;
+    private long sum;
+    private int min;
+    private int max;
+
     public Stats(final long countValue,
                  final long sumValue,
                  final int minValue,
@@ -12,22 +17,19 @@ public class Stats {
         max = maxValue;
     }
 
-    private long count;
+
     public final long getCount() {
         return count;
     }
 
-    private long sum;
     public final long getSum() {
         return sum;
     }
 
-    private int min;
     public final int getMin() {
         return min;
     }
 
-    private int max;
     public final int getMax() {
         return max;
     }
